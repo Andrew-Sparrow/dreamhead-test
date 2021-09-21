81a7f03b62112ae3b3003a57103b868f6eccbb49
Andrew <8911102@gmail.com>
2021-09-21 21:42:48 +0500

    logo was changed with text

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -19,9 +19,7 @@ function Layout(props) {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <Link className="header__logo-link header__logo-link--active" to="/">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </Link>
+              <h2>Contacts</h2>
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
