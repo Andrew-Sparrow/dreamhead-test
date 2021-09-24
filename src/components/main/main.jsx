ffb79f0614fc238a2faff264e2c413b9eaf59f56
Andrew <8911102@gmail.com>
2021-09-24 08:20:11 +0500

    places were renamed to contacts in main.jsx

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -10,23 +10,23 @@ import {getContacts, getActiveGroupName} from '../../store/places/selectors';
 
 function Main() {
   const activeGroupName = useSelector(getActiveGroupName);
-  const places = useSelector(getContacts);
+  const contacts = useSelector(getContacts);
 
-  const filteredPlaces = Util.getFilteredPlaces(activeGroupName, places);
+  const filteredContacts = Util.getFilteredPlaces(activeGroupName, contacts);
 
   return (
     <main className="page__main page__main--index">
       <h1 className="visually-hidden">Cities</h1>
       <Tabs />
       {
-        filteredPlaces.length === 0
+        filteredContacts.length === 0
           ? <MainEmpty activeGroupName={activeGroupName}/>
           : (
             <div className="cities">
               <div className="cities__places-container container">
                 <section className="cities__places places">
-                  <h2 className="visually-hidden">Places</h2>
-                  <b className="places__found">{filteredPlaces.length} places to stay in {activeGroupName}</b>
+                  <h2 className="visually-hidden">Contacts</h2>
+                  <b className="places__found">{filteredContacts.length} places to stay in {activeGroupName}</b>
                   {/* <RoomList places={filteredPlaces} /> */}
                 </section>
               </div>
