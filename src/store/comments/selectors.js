ece27bdc833af2c65c84accec49ddebacf4f743f
Andrew <8911102@gmail.com>
2021-09-24 16:27:00 +0500

    comments were deleted from store

--- src/store/comments/selectors.js
+++
@@ -1,4 +0,0 @@
-import {NameSpace} from '../root-reducer';
-
-export const getComments = (state) => state[NameSpace.COMMENTS].comments;
-export const getIsCommentsLoaded = (state) => state[NameSpace.COMMENTS].isCommentsLoaded;
